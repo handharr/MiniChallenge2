3aedab1a023144ca52975e25db1d664e08574182
Handhar <phandharmahua@gmail.com>
2021-06-02 12:16:59 +0700

    initial commit

---
+++ MiniChallenge2/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  MiniChallenge2
+//
+//  Created by Puras Handharmahua on 02/06/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
