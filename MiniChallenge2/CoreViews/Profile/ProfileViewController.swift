318510991a55ecfda7bd8ca41a5dfd078112e4ea
Jackie Leonardy <jackiezheng2024@gmail.com>
2021-06-14 13:34:49 +0700

    Adding Profile Page and Watch Connectivity Observer

--- MiniChallenge2/CoreViews/Profile/ProfileViewController.swift
+++ MiniChallenge2/CoreViews/Profile/ProfileViewController.swift
@@ -2,7 +2,7 @@
 //  ProfileViewController.swift
 //  MiniChallenge2
 //
-//  Created by Puras Handharmahua on 03/06/21.
+//  Created by Jackie Leonardy on 14/06/21.
 //
 
 import UIKit
@@ -11,8 +11,19 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "Profile"
-        view.backgroundColor = .systemTeal
+
+        // Do any additional setup after loading the view.
     }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
